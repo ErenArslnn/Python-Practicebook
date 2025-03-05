eee631d27c52923bda07ce2bfa97be7914e025c3
eren_arslan <erenarslan365@gmail.com>
2025-03-05 22:48:30 +0300

    data-types worksheet file commit

---
+++ data-types.py
@@ -0,0 +1,19 @@
+name = "Eren"
+age = 23
+weight = 75.2
+isStudent = True
+
+if isStudent == True:
+    isStudent = "Yes"
+
+else:
+    isStudent = "No"
+
+    
+
+print(type(name))
+print(type(age))
+print(type(weight))
+print(type(isStudent))
+
+print(f"Name: {name} Age: {age} Weight: {weight} Is student: {isStudent}")
