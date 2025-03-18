04cea5b81c31fd6030d9c0fdad01f9b39ed95e5a
eren_arslan <erenarslan365@gmail.com>
2025-03-18 23:10:46 +0300

    an entry to dictionaries committed

---
+++ basic-studies/03.lists/dictionary-studies.py
@@ -0,0 +1,11 @@
+# dictionary only accepts unique keys in itself
+phoneCodes = {
+    90: 'Turkey',
+    44: 'UK',
+    30: 'Greece',
+    34: 'France',
+    34: 'Spain'
+}
+
+print(phoneCodes)
+# outcome wont include france as key : 34 is overwritten to Spain 
