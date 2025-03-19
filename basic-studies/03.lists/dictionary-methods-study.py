8b5659ade84f739c13bdd52e2e0c54f38fc82141
eren_arslan <erenarslan365@gmail.com>
2025-03-20 02:27:04 +0300

    update and delete item methods added.

--- basic-studies/03.lists/dictionary-methods-study.py
+++ basic-studies/03.lists/dictionary-methods-study.py
@@ -15,5 +15,18 @@ output = videoGames.items()
 
 # update items
 videoGames["Genre"] = "Open-world Role-Playing Action Adventure"
+videoGames.update({"Game Name":"Skyrim"})
+output = videoGames
+
+# delete items
+videoGames.pop("Genre")  # Deletes spesified key, value pair.
+videoGames.popitem()     # Deletes last added key, value pair.
+videoGames.clear()       # Deletes all items in the dictionary.
+
+
+
+
+
+
 output = videoGames
 print(output)
