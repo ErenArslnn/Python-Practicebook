ad8e607a6d6a5e1b43e6387164199f8e361f7ebc
eren_arslan <erenarslan365@gmail.com>
2025-03-19 18:34:51 +0300

    an easy solution to dictionary study application is added misbehaving 'if' conditions removed.

--- basic-applications/dictionaries-application.py
+++ basic-applications/dictionaries-application.py
@@ -16,15 +16,39 @@ print(countryInfo)
 # print dynamic sentence using key pairs from the dictionary list
 
 keyboardInput = input("Country: ")
-if keyboardInput != countryInfo.keys():
-    print("Wrong input. Available countries: 'Greece','Turkey','Bulgaria','Romania'")
+
 
 if keyboardInput == 'Turkey':
-    print(f"{keyboardInput} located at {countryInfo['Turkey'][1]} and its phone code is {countryInfo['Turkey'][0]}")
+    print(f"{keyboardInput} located in {countryInfo['Turkey'][1]} and its phone code is {countryInfo['Turkey'][0]}")
 elif keyboardInput == 'Greece':
-     print(f"{keyboardInput} located at {countryInfo['Greece'][1]} and its phone code is {countryInfo['Greece'][0]}")
+     print(f"{keyboardInput} located in {countryInfo['Greece'][1]} and its phone code is {countryInfo['Greece'][0]}")
 elif keyboardInput == 'Bulgaria':
-    print(f"{keyboardInput} located at {countryInfo['Bulgaria'][1]} and its phone code is {countryInfo['Bulgaria'][0]}")
+    print(f"{keyboardInput} located in {countryInfo['Bulgaria'][1]} and its phone code is {countryInfo['Bulgaria'][0]}")
 elif keyboardInput == 'Romania':
-    print(f"{keyboardInput} located at {countryInfo['Romania'][1]} and its phone code is {countryInfo['Romania'][0]}")
+    print(f"{keyboardInput} located in {countryInfo['Romania'][1]} and its phone code is {countryInfo['Romania'][0]}")
 
+# it seems this is the long way to do it. We can also define dictionaries as values. Here is the more efficent way:
+
+countryInfo2 = {
+    "Greece": {
+        "Phone Code": 30,
+        "Continent": "Europe"
+    },
+    "Turkey": {
+        "Phone Code": 90,
+        "Continent": "Asia"
+    },
+    "Bulgaria": {
+        "Phone Code": 359,
+        "Continent": "Europe"
+    },
+    "Romania": {
+        "Phone Code": 40,
+        "Continent": "Europe"
+    }
+}
+countryName = input("Country: ")
+
+country = countryInfo2[countryName]
+
+print(f"{countryName} is located in {country["Continent"]} and its phone code is: {country["Phone Code"]}")
