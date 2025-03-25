cce76a649c9fcc2627bf9505b4f1a2d0fc5d10f0
eren_arslan <erenarslan365@gmail.com>
2025-03-26 00:06:48 +0300

    basic study on operators committed

---
+++ basic-studies/04.operators/basic-operators-study.py
@@ -0,0 +1,20 @@
+# we have some basic operators let us do some calculations, concating, dividing and more.
+
+x = 10
+y = 20
+
+sum = x + y
+subtract = x - y
+multiply = x * y
+divide = x / y
+mode = x % y        # ===> Useful with coding questions such as 'fizzBuzz'.  
+exponential = x ** y
+intDivide = x // y  # ===> only prints division value of integers without mod value. for example: 'print(5 // 2)', will print the integer '2'.
+
+print(sum)
+print(subtract)
+print(multiply)
+print(divide)
+print(mode)
+print(exponential)
+print(intDivide)
