6656e331a2e315dcc00699d21f60662a9713786f
eren_arslan <erenarslan365@gmail.com>
2025-04-05 23:24:03 +0300

    a basic study on compare operators has been covered.

---
+++ basic-studies/04.operators/compare-operators-study.py
@@ -0,0 +1,28 @@
+a,b,c,d = 1,1,10,5
+
+result = (a == b)
+
+print(result)
+
+result = (a < b)
+
+print(result)
+
+result = (c != a)
+
+print(result)
+
+# email = "info@erenarslan.com"
+# password = "123456789"
+
+# result = (email == input("email:"))
+
+print(result)
+
+result = (int(True))
+
+print(result)
+
+result = (int(False))
+
+print(result)
