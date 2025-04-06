3d333d726e3696939632f64ef128cbc71e2059dc
eren_arslan <erenarslan365@gmail.com>
2025-04-06 19:50:29 +0300

    mistakes on logical errors because of wrong use of 'or' has been fixed.

--- basic-studies/04.operators/logical-operators-study.py
+++ basic-studies/04.operators/logical-operators-study.py
@@ -2,16 +2,16 @@
 # Lets say a candidate has to be over 18 and at least high school graduate.
 
 inputAge = int(input("Age of the candidate: "))
-inputEducation = input("Education level (high school or university degree allowed only): ")
+inputEducation = input("Education level (high school or university degree allowed only): ").strip().lower()
 
-if inputAge >= 18 and inputEducation == "High school" or "high school" or "university" or "University":
-    print("Candidate is suitable for driving license.")
+if inputAge >= 18 and (inputEducation == "high school" or inputEducation == "university"):
+    print("Candidate is suitable for a driving license.")
 
-elif inputAge < 18 and inputEducation == "High school" or "high school" or "university" or "University":
-    print(f"Candidate is not suitable for driving license. Because age {inputAge} is not suitable.")
+elif inputAge < 18:
+    print(f"Candidate is not suitable for a driving license. Because age {inputAge} is not suitable.")
 
-elif inputEducation != "High school" or "high school" or "university" or "University" and inputAge >= 18:
-    print(f"Candidate is not suitable for driving license. Because education level {inputEducation} is not suitable.")
+elif inputEducation not in ["high school", "university"]:
+    print(f"Candidate is not suitable for a driving license. Because education level '{inputEducation}' is not suitable.")
 
-elif not (inputAge >= 18 and inputEducation == "High school" or "high school" or "university" or "University"):
-    print(f"Candidate is not suitable for driving license. Because education level {inputEducation} and age {inputAge} is not suitable.")
+else:
+    print(f"Candidate is not suitable for a driving license. Because education level '{inputEducation}' and age {inputAge} are not suitable.")
