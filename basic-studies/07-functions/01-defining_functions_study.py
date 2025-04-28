a2850c6a15e806df77d32d3a8d959f2bd0972723
eren_arslan <erenarslan365@gmail.com>
2025-04-29 01:25:39 +0300

    Add initial function definitions and basic application examples

---
+++ basic-studies/07-functions/01-defining_functions_study.py
@@ -0,0 +1,19 @@
+# In this study we will learn the basics of defining functions and how to use them:
+
+# Defining a function:
+def greet():
+    print("Hello World")
+
+# Using a function:
+greet()
+
+
+# Basic application:
+def mySum():
+    print("a + b calculator")
+    a = int(input("a :"))    
+    b = int(input("b :"))
+    print(a + b)
+
+mySum()    
+
