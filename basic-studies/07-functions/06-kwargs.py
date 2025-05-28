6e881f35c003ad09ec1803bc08a516ccfb5dfc0c
eren_arslan <erenarslan365@gmail.com>
2025-05-28 17:08:13 +0300

    Add implementation of loginInfo function to demonstrate handling keyword arguments

---
+++ basic-studies/07-functions/06-kwargs.py
@@ -0,0 +1,10 @@
+# In this file, we will handle key word arguments, kwargs in short.
+
+# Goal is the same with '*args'. We want to sent custom amount of paramters in our function but this time, we will be working with key value types.
+
+def loginInfo(**kwargs):
+    for key,value in kwargs.items():
+        print(f"{key}: {value} \n")
+
+loginInfo(username = 'lionellmessi', email = 'messi@goat.com', password = '123messi123')
+
